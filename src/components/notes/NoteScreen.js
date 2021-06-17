085a5de7f35652e2ca016a540b58272cc9383359
giobanny23 <edgar_2_62@hotmail.com>
2021-06-16 23:23:47 -0500

    Estructura y Designe del HomeScreen

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -5,6 +5,31 @@ export const NoteScreen = () => {
     return (
         <div className="note__main-content">
             <NotesAppBar/>
+
+            <div className="notes__content">
+                <input
+                    type="text"
+                    placeholder="Some awesome title"
+                    className="notes__title-input"
+                    autoComplete="off"
+                />
+
+            <textarea
+                placeholder="What Happened Today"
+                className="notes__textarea"
+            >
+                    
+            </textarea>
+
+            <div className="notes__image">
+                <img
+                    src="https://i.pinimg.com/564x/76/e2/62/76e262a7eadf680f116db013f8dfa59b.jpg"
+                    alt="imagen"
+                />
+            </div>
+
+
+            </div>
         </div>
     )
 }
