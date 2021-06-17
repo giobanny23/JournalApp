54ee16217a20208502488c318cd80aee3e98b0a3
giobanny23 <edgar_2_62@hotmail.com>
2021-06-16 22:52:43 -0500

    Componente de NoteAppBar

--- src/components/journal/JournalScreen.js
+++ src/components/journal/JournalScreen.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Sidebar } from './Sidebar';
-import { NothingSelected } from './NothingSelected';
+import { NoteScreen } from '../notes/NoteScreen';
+//import { NothingSelected } from './NothingSelected';
 
 
 export const JournalScreen = () => {
@@ -10,7 +11,8 @@ export const JournalScreen = () => {
     <Sidebar/>
 
         <main>
-            <NothingSelected />
+            {/*<NothingSelected /> */}
+            <NoteScreen/>
 
         </main>
 
