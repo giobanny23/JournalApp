2e41494674fc9717ee192c00bea2a1746d6cb642
giobanny23 <edgar_2_62@hotmail.com>
2021-06-17 23:29:45 -0500

    Manejo de Formulario

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,12 +1,38 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import { useForm } from '../../hooks/useForm';
 
 export const RegisterScreen = () => {
+
+  const [ formValues, handleInputChange] = useForm({ 
+      name: 'giobanny',
+      email: 'nando2@gmail.com',
+      password: '123456',
+      passsword2:'123456'
+  })
+
+  const { name,email,password, passsword2 } = formValues;
+
+  const handleRegister = (e) => {
+      e.preventDefault();
+
+      if( isFormValid()){
+          console.log('Formulario Correcto');
+      }
+      
+  }
+
+  const isFormValid =() =>{
+
+  }
+
+
+
     return (
         <>
         <h3 className ="auth__title">Register</h3>
 
-        <form>
+        <form onSubmit ={handleRegister}>
 
             <input
                 type="text"
@@ -14,6 +40,8 @@ export const RegisterScreen = () => {
                 name="name"
                 className="auth__input"
                 autoComplete="off"
+                value={name}
+                onChange={handleInputChange}
             />
 
 
@@ -23,6 +51,8 @@ export const RegisterScreen = () => {
                 name="email"
                 className="auth__input"
                 autoComplete="off"
+                value={email}
+                onChange={handleInputChange}
             />
 
             <input
@@ -30,12 +60,16 @@ export const RegisterScreen = () => {
                 placeholder ="Password"
                 name="password"
                 className="auth__input"
+                value={password}
+                onChange={handleInputChange}
             />
             <input
                 type="password"
                 placeholder ="Confirm Passsword"
                 name="password2"
                 className="auth__input"
+                value={passsword2}
+                onChange={handleInputChange}
             />
 
             <button
