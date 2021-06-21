cb6ada16eadf1b34af32f85b55b379d7937ee8fe
giobanny23 <edgar_2_62@hotmail.com>
2021-06-20 23:09:32 -0500

    autentificacion de usuario

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import {
     BrowserRouter as Router,
@@ -6,10 +6,33 @@ import {
     Route,
     Redirect
   } from "react-router-dom";
+
+import { useDispatch } from 'react-redux';
+
+import {firebase} from '../firebase/firebase-config';  
 import { JournalScreen } from '../components/journal/JournalScreen';
 import { AuthRouter } from './AuthRouter';
+import { login } from '../actions/auth';
 
 export const AppRouter = () => {
+
+    const dispatch = useDispatch();
+
+
+    useEffect(() => {
+
+        firebase.auth().onAuthStateChanged( (user) =>{
+
+            if(user?.uid){
+                dispatch(login(user.uid, user.displayName));
+
+            }
+        });
+    
+    },[])
+
+
+
     return (
         <Router>
 
