490cc0eb1b375df5c9f91a44e9ea67ca4fef9852
giobanny23 <edgar_2_62@hotmail.com>
2021-07-24 23:36:16 -0500

    agregando variables de entorno con testing

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -3,6 +3,8 @@ import  'firebase/firestore';
 import 'firebase/auth';
 
 
+console.log(process.env);
+
 const firebaseConfig = {
     apiKey: "AIzaSyAHzt1kdbS6Du_xgmc9tIiq7-DLqwW23-w",
     authDomain: "react-app-cursos-ed840.firebaseapp.com",
@@ -11,8 +13,31 @@ const firebaseConfig = {
     messagingSenderId: "174049190710",
     appId: "1:174049190710:web:dc5e9a4c519dc1c10c8997"
   };
+
+  const firebaseConfigTesting = {
+    apiKey: "AIzaSyC_CAmKmqzesxPHroFUQJ8FeEaIEGiI-kg",
+    authDomain: "proyecto2019-548ec.firebaseapp.com",
+    databaseURL: "https://proyecto2019-548ec.firebaseio.com",
+    projectId: "proyecto2019-548ec",
+    storageBucket: "proyecto2019-548ec.appspot.com",
+    messagingSenderId: "419747290417",
+    appId: "1:419747290417:web:4bd1d86d140a1be0852410"
+  };
+
+
+
+  if ( process.env.NODE_ENV === 'test'){
+      //testing
+      firebase.initializeApp(firebaseConfigTesting);
+  } else {
+      //deve/prod
+      firebase.initializeApp(firebaseConfig);
+      
+  }
+
+
+
   // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
 
   const db = firebase.firestore();
   const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
